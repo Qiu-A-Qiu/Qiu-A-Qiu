09e414d76175eaba651cbb1f522616f905196c53
Qiu-A-Qiu <qjygongzuo@163.com>
2024-09-09 16:35:42 +0800

    Update M5StickCPlus2_slot.ino

--- M5StickCPlus2_slot.ino
+++ M5StickCPlus2_slot.ino
@@ -1,12 +1,4 @@
-#define Plus 0
-#define Automation 0
-#if Plus
-  #include "M5StickCPlus.h"
-#else
-  #include "M5StickCPlus2.h"
-  #include "time.h"
-#endif
-
+#include <M5StickCPlus2.h>
 #include "Slot.h"
 
 #ifndef _countof
@@ -15,85 +7,53 @@
 
 #define LOOP_WAIT 30
 #define FLUSH_DELAY 100
-#define FLUSH_COUNT 5
-
-#define Time_MAX 80
-#define Time_MIN 45
-#define InitTime_MAX 100
-#define InitTime_MIN 80
-#define StartCount_MAX 6
+#define FLUSH_COUNT 3
 
 Slot slots[SLOT_COUNT];
-const int symbolIndices[] = { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
+const int symbolIndices[] = { 2, 4, 5, 0, 3, 4, 2, 5, 3, 1, 0};//列图标显示顺序  每一列显示的图标相同
+
 enum SlotsState { SLOTS_INIT, SLOTS_START, SLOTS_STOP = SLOT_COUNT + 1, SLOTS_FLUSH };
-int state = SLOTS_INIT;
+int state = SLOTS_INIT;//老虎机状态值     设置为初始状态
 
 //
-// setup()
+// setup()//初始化函数
 //
-
 void setup() {
  	M5.begin();
 	M5.Lcd.setRotation(3);
-	M5.Lcd.fillScreen(TFT_BLACK);
+	M5.Lcd.fillScreen(TFT_BLACK);//设置屏幕背景色为黑色
 	M5.Lcd.setSwapBytes(true);
-  
-  #if Plus
-	M5.Axp.ScreenBreath(12);
-  #endif
-  
-	Slot::initShadow();
-	Slot::setReel(symbolIndices, _countof(symbolIndices));
+
+
+	Slot::initShadow();//初始化阴影
+	Slot::setReel(symbolIndices, _countof(symbolIndices));//设置初始转盘
 	for (int i = 0; i < SLOT_COUNT; i++) {
-		slots[i].init(i, i * SLOT_COUNT);//state = SLOT_INIT;
+		slots[i].init(i, i * SLOT_COUNT);
 		slots[i].draw();
 	}
- srand(time(0));
 }
 
 //
 // loop()
 //
-
 void loop() {
 	unsigned long tick = millis();
 	static unsigned long flushTick;
 	static int flushCount;
-  static int Slot_Start = 0;
-  static int Slot_Stop = 0;
-  static int Count_Stop = 0;
-  static int Count_Num = rand() % (InitTime_MAX - InitTime_MIN - 1) + InitTime_MIN;
-  if(Automation){    
-  	M5.update();
-  	if (M5.BtnA.wasPressed()||Slot_Stop == 1) {
-  		if (state == SLOTS_INIT) {
-  			for (int i = 0; i < SLOT_COUNT; i++) {
-  				slots[i].start();
-  			}
-  			state++;
-        Slot_Start = 1;
-        Count_Num = rand() % (InitTime_MAX - InitTime_MIN - 1) + InitTime_MIN;
-  		} else if (state < SLOTS_STOP) {          
-        Slot_Start++;
-  			slots[state - 1].stop();    
-        state++;
-  		}
-  	}
-    Slot_Stop = 0;
-  }else{
-    M5.update();
-    if (M5.BtnA.wasPressed()) {
-      if (state == SLOTS_INIT) {
-        for (int i = 0; i < SLOT_COUNT; i++) {
-          slots[i].start();
-        }
-        state++;
-      } else if (state < SLOTS_STOP) {
-        slots[state - 1].stop();
-        state++;
-      }
-  }  
-  }
+
+	M5.update();
+
+	if (M5.BtnA.wasPressed()) {
+		if (state == SLOTS_INIT) {
+			for (int i = 0; i < SLOT_COUNT; i++) {
+				slots[i].start();
+			}
+			state++;
+		} else if (state < SLOTS_STOP) {
+			slots[state - 1].stop();
+			state++;
+		}
+	}
 	if (state == SLOTS_STOP) {
 		int symbol = -1;
 		bool stopAll = true;
@@ -126,18 +86,7 @@ void loop() {
 			}
 		}
 	}
- if(Automation){
-   if(Slot_Start && Slot_Start < StartCount_MAX){
-    Count_Stop++;
-    if((Count_Stop == Count_Num)&&state < SLOTS_STOP){
-      Slot_Stop = 1;
-      Count_Stop = 0;
-      Count_Num = rand() % (Time_MAX - Time_MIN - 1) + Time_MIN;
-    }
-   }else if(Slot_Start == StartCount_MAX){
-     Slot_Start = 0;
-   }
- }
+
 	for (int i = 0; i < SLOT_COUNT; i++) {
 		if (slots[i].update()) {
 			slots[i].draw();
@@ -148,5 +97,4 @@ void loop() {
 	if (ms < LOOP_WAIT) {
 		delay(LOOP_WAIT - ms);
 	}
- 
 }
